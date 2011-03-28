aefbf2c47ab16c93ddb735ae7d1aec5574e24cee
Doug Burkhalter <dougtheneard@gmail.com>
2011-03-28 13:43:40 -0500

    More Updates, UI Refinements, Added Tips

--- src/net/itjustwerx/recyclebuddy/search.java
+++ src/net/itjustwerx/recyclebuddy/search.java
@@ -1,10 +1,22 @@
 package net.itjustwerx.recyclebuddy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Debug;
 import android.text.Editable;
 import android.view.View;
+import android.view.Window;
 import android.view.View.*;
 import android.widget.*;
 
@@ -17,6 +29,7 @@ public class search extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
         
         //Initialize needed widgets
@@ -28,6 +41,7 @@ public class search extends Activity {
         final CheckBox cbAluminum = (CheckBox) findViewById(R.id.cbAluminum);
         final EditText etCityState = (EditText) findViewById(R.id.etCityState);
         final EditText etZipCode = (EditText) findViewById(R.id.etZipCode);
+        final TextView tvTips = (TextView) findViewById(R.id.tvTips);
         
         OnClickListener oclOtherCheckBoxes = new OnClickListener() {
         	public void onClick(View v) {
@@ -48,5 +62,40 @@ public class search extends Activity {
         cbCardboard.setOnClickListener(oclOtherCheckBoxes);
         cbPaper.setOnClickListener(oclOtherCheckBoxes);
         cbAluminum.setOnClickListener(oclOtherCheckBoxes);
+        
+        BufferedReader in = null;
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI("http://recyclebuddy.itjustwerx.net/gettip.php"));
+            HttpResponse response = client.execute(request);
+            in = new BufferedReader
+            (new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+            String page = sb.toString();
+            System.out.println(page);
+            tvTips.setText(page);
+            } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				
+				e.printStackTrace();
+			} finally {
+            if (in != null) {
+                try {
+                    in.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+		}
     }
 }
